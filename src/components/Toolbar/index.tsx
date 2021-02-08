bd0870d39f87abd27731eb033a9fa6467a3845b0
iWuzhi <2461358597@qq.com>
2021-02-08 11:35:33 +0800

    refactor(Toolbar):  Replace dumb code with router params

--- src/components/Toolbar/index.tsx
+++ src/components/Toolbar/index.tsx
@@ -4,6 +4,7 @@
  */
 
 import React from 'react';
+import { NavLink as Link, useParams } from 'react-router-dom';
 import { Button } from 'gk-rc';
 
 interface IProps {
@@ -11,10 +12,18 @@ interface IProps {
 }
 
 const Toolbar: React.FC<IProps> = ({ actions }) => {
+  const { category, section } = useParams<{
+    category: string;
+    section: string;
+  }>();
   return (
     <div className="flex border-b p-3 border-gray-300">
-      <h3 className="flex-grow flex items-center text-green-300 font-serif italic font-semibold tracking-wide">
-        Sort / Inset Sort
+      <h3 className="flex-grow flex items-center font-serif italic font-semibold tracking-wide">
+        <Link to={`/${category}`} activeClassName="text-green-300 text-2xl">
+          {category}
+        </Link>
+        <span className="px-2 text-gray-400">/</span>
+        <span className="text-sm text-gray-500 self-end pb-1">{section}</span>
       </h3>
       <div>
         {actions.map((action, index) => {
